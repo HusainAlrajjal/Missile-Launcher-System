c7575f44ad99a3ddcda06a25b091a0aa7a609fc0
moh2099 <moh_2323@hotmail.com>
2020-12-05 22:05:43 +0300

    asd

--- main.py
+++ main.py
@@ -10,11 +10,12 @@ def save_keys(keys):
     pwList.close()
 
 
+print("\n------------------------Setup The System------------------------\n")
+min_digits = int(input('Enter Min digits: '))
+k = int(input('Enter threshold (k): '))            # threshold
+n = int(input('Enter the number of generals (n): '))  # number of keys to be generated
 
 print("\n------------------------Staff Only------------------------")
-min_digits = int(input('Enter Min digits>>'))
-k = int(input('Enter threshold (k) >>'))            # threshold
-n = int(input('Enter the number of generals (n) >>'))   # number of keys to be generated
 x, MList, keys = CRT_Setup(n, min_digits)
 print("TOP secret (N)\t\t:", x)
 print("TOP secret length (N)\t\t:", len(str(x)) ," digits")
